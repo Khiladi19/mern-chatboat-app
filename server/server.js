8ab66afa78e4b102e04a9b26871c97499c06a43a
unknown <abhiyadav20193@.com>
2025-06-14 12:44:15 +0530

    update file

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   // origin: "http://localhost:5173", 
-  origin: "https://mern-chatboat-frontend.onrender.com", 
+  origin: "https://mern-chatboat-app-frontend.onrender.com", 
   credentials: true,
 }));
 // Routes
